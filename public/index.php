2ec13c35fe85f321c54049c1bfed57c1651d639a
snarpeppo <snarpeppo98@gmail.com>
2021-10-01 09:43:47 +0200

    initial refactoring of homepage

--- public/index.php
+++ public/index.php
@@ -1,24 +1,169 @@
-<html>
-
+<html lang="it" dir="ltr">
 <head>
-    <?php include('template/headContent.php') ?>
+
+    <!-- META TAG -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- TITLE -->
+    <title>HomePage Gabbo</title>
+
+    <!-- GOOGLE FONTS 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+    -->
+
+    <!-- BOOTSTRAP CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <!-- BOX ICONS -->
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- STILE -->
+    <link rel="stylesheet" href="style/style2.css">
+
 </head>
 
-<body class='bg-light'>
-    <?php include('template/navContent.php') ?>
-    <div class="container-sm mx-auto">
-        <div class="row">
-            <div class="bg-white col-10 col-sm-6 mx-auto text-center mt-5 py-4 shadow">
-                <h3 class="text-primary text-center"> Want to make a submission?</h3>
-                <div class="justify-content-center">
-                    <a class="btn btn-primary mt-3 bg-primary" href="submission.php">Click Here!</a>
+<body>
+
+    <div class="sidebar">
+        <div class="logo_content">
+            <div class="logo">
+                <div class="logo_name">
+                    <img src="img/finsoft-logo.png" alt="" class="img-fluid">
                 </div>
+            </div>
+            <i class='bx bx-menu' id="btn"></i>
+        </div>
+        <ul class="nav_list">
+            <li>
+
+                <i class='bx bx-search' ></i>
+                <input type="text" placeholder="Search">
+
+                <span class="tooltip">Dashboard</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-user'></i>
+                    <span class="links_name">User</span>
+                </a>
+                <span class="tooltip">User</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-message-dots' ></i>
+                    <span class="links_name">Messages</span>
+                </a>
+                <span class="tooltip">Messages</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-stats' ></i>
+                    <span class="links_name">Status submissions</span>
+                </a>
+                <span class="tooltip">Status submissions</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-plus-circle'></i>
+                    <span class="links_name">New submission</span>
+                </a>
+                <span class="tooltip">New submission</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-cog' ></i>
+                    <span class="links_name">Settings</span>
+                </a>
+                <span class="tooltip">Settings</span>
+            </li>
+        </ul>
+        <div class="profile_content">
+            <div class="profile">
+                <div class="profile_details">
+                    <img src="./img/gabbo.jpg" alt="">
+                    <div class="name_job">
+                        <div class="name">Gabriele Battistata</div>
+                        <div class="job">Junior Web-app</div>
+                    </div>
+                </div>
+                <i class='bx bx-log-out' id="log_out"></i>
             </div>
         </div>
     </div>
-    <footer class="footer fixed-bottom mt-auto py-3 bg-light shadow-lg">
-        <?php include('template/footerContent.php') ?>
-    </footer>
-</body>
 
+    <div class="home_content m-2">
+        <div class="d-flex flex-wrap justify-content-between">
+            <div class="card" style="width: 17rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+              <div class="card" style="width: 17rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+              <div class="card" style="width: 17rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+              <div class="card" style="width: 17rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+              <div class="card" style="width: 17rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+              <div class="card" style="width: 17rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+              <div class="card" style="width: 17rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+              <div class="card" style="width: 17rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+              <div class="card" style="width: 17rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+        </div>
+    </div>
+
+
+    <script src="script/main.js"></script>
+    
+</body>
 </html>
