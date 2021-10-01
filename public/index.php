bb5343742fdb80e9fc9e3751b24620e74689d3a7
alessioM <axel3642@gmail.com>
2021-10-01 09:10:55 +0200

    fix style and navbar

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@
     <?php include('template/navContent.php') ?>
     <div class="container-sm mx-auto">
         <div class="row">
-            <div class="bg-white col-sm-6 mx-auto text-center mt-5 py-4 shadow">
+            <div class="bg-white col-10 col-sm-6 mx-auto text-center mt-5 py-4 shadow">
                 <h3 class="text-primary text-center"> Want to make a submission?</h3>
                 <div class="justify-content-center">
                     <a class="btn btn-primary mt-3 bg-primary" href="submission.php">Click Here!</a>
--- public/template/navContent.php
+++ public/template/navContent.php
@@ -1,7 +1,9 @@
-<nav class="navbar navbar-expand-sm navbar-dark bg-primary justify-content-center shadow" id="navbar">
+<nav class="navbar navbar-dark bg-primary justify-content-center shadow" id="navbar">
     <ul class="navbar-nav">
         <li class="nav-link active">
-            <a href="../public/index.php" class="text-white text-decoration-none fs-1"> Content Manager </a>
+            <a href="../public/index.php" class="text-white text-decoration-none fs-1"> 
+                Content Manager 
+            </a>
         </li>
     </ul>
 </nav>
